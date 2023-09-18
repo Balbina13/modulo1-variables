6b1e5df2e4ed977b94b3064a0fb97bbe831757d4
Ruth Balbina Figueredo <ruth_lestat@hotmail.com>
2023-09-18 03:59:47 -0300

    second exercise finish

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Ejercicio 1:
 //a- declarar variables con datos de usuario (user, age, email)
 const user = "Rosa Montero"
-const age = Number(31)
+let age = Number(31)
 const email =  "terrorhouse@gmail.com"
 
 //b- mostrar estos datos en la consola
@@ -14,6 +14,18 @@ console.log(typeof user)
 console.log(typeof age)
 console.log(typeof email)
 
+//Ejercicio 2:
+//a- pedir al usuario sus datos personales (nombre, edad, pais, profesion)
+const username = prompt("Ingrese su nombre:")
+age = Number(prompt("Ingrese su edad:"))
+const country = prompt("Ingrese su pais:")
+const profession = prompt("Ingrese su profesion:")
+console.log(username)
+//b- mostrar esos datos por la consola usando concatenacion
+console.log( username + " " + age + " " + country + " " + profession)
+
+//c- mostrar en consola la edad + 10
+console.log(age + 10)
 
 
 
