094feac8907b4890b4b5c2a077fe5d49b211c3ec
Ruth Balbina Figueredo <ruth_lestat@hotmail.com>
2023-09-18 03:11:27 -0300

    first exercise finish

---
+++ index.js
@@ -0,0 +1,19 @@
+//Ejercicio 1:
+//a- declarar variables con datos de usuario (user, age, email)
+const user = "Rosa Montero"
+const age = Number(31)
+const email =  "terrorhouse@gmail.com"
+
+//b- mostrar estos datos en la consola
+console.log(user)
+console.log(age)
+console.log(email)
+
+//c- mostrar el tipo de dato de cada variable por consola
+console.log(typeof user)
+console.log(typeof age)
+console.log(typeof email)
+
+
+
+
